8302c5ca1c00dd91530c93da49a5f44b4d280a07
solowihmaria <solowihmaria@gmail.com>
2025-03-23 12:01:39 +0300

    feat: add solution of 5th task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -90,8 +90,14 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...coefficients) {
+  if (coefficients.length === 0) return null;
+
+  return (x) =>
+    coefficients.reduce(
+      (sum, coef, index) => sum + coef * x ** (coefficients.length - 1 - index),
+      0
+    );
 }
 
 /**
