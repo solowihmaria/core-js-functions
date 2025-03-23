cb5b17b919f76ad30645bbb58f8bb85576a35d59
solowihmaria <solowihmaria@gmail.com>
2025-03-23 12:17:28 +0300

    feat: add solution of 9th task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -208,8 +208,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function arg(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
